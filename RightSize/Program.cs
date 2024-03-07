f893a106de0f7dccfa53842768532263bc8eba91
João <joaombmrodrigues@gmail.com>
2024-03-07 23:43:10 +0000

    Caso algum argumento tenha comprimento superior a 6 caráteres, o programa termina imediatamente sem imprimir esse argumento

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -7,11 +7,17 @@ namespace RightSize
         static void Main(string[] args)
         {
             //verificar cada argumento dado na linha de comandos
-            foreach (string Str in args)
+            foreach (string s in args)
                 //mostrar no ecra se comprimento for superior a 3 carateres
-                if(Str.Length > 3)
+                //e inferior a 6 carateres
+                if(s.Length > 3 & s.Length<6)
                 {
-                    Console.WriteLine(Str);
+                    Console.WriteLine(s);
+                }
+                //sair do programa se o comprimento for maior que 6 carateres
+                else if(s.Length >6)
+                {
+                    return;
                 }
         }
     }
