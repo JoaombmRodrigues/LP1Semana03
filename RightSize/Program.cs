eff940e83d4fc1d49d95aa16c2f1955d65f76b8c
João <joaombmrodrigues@gmail.com>
2024-03-07 23:24:51 +0000

    Programa que mostra no ecrã os argumentos da linha de comandos com comprimento superior a 3 caráteres

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //verificar cada argumento dado na linha de comandos
+            foreach (string Str in args)
+                //mostrar no ecra se comprimento for superior a 3 carateres
+                if(Str.Length > 3)
+                {
+                    Console.WriteLine(Str);
+                }
+        }
+    }
+}
