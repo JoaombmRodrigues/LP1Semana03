00f66c6f7c3585691ac26f5cc297f9b56522fb66
João <joaombmrodrigues@gmail.com>
2024-03-07 23:57:23 +0000

    Cria um programa que solicita uma string e um carater ao utilizador

---
+++ ChangeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for string
+            Console.WriteLine("Write a string: ");
+            string s = Console.ReadLine();
+            //ask for a character
+            Console.WriteLine("Write a character: ");
+            string s2 = Console.ReadLine();
+            char c = char.Parse(s2);
+        }
+    }
+}
