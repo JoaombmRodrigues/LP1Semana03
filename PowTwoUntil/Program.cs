ebb8504ebe2fce12a1c72b37c9391b5ad36faddb
João <joaombmrodrigues@gmail.com>
2024-03-08 11:52:55 +0000

    Cria um programa 'PowTwoUntil' e declara o metodo 'PowersOf2Until5'

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void PowersOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
