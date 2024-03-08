d428122d111328ed39bdd353a9770f16bf42af8d
João <joaombmrodrigues@gmail.com>
2024-03-08 18:35:05 +0000

    Mostra no ecrã os poderes esccolhidos por cada um dos jogadores

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PlayerPowers
 {
@@ -10,7 +11,44 @@ namespace PlayerPowers
             Console.Write("Qual o número de jogadores? ");
             string tempstr = Console.ReadLine();
             int n = int.Parse(tempstr);
-            
+
+            //create array
+            Powers [] pwrs = new Powers[n];
+            //check powers
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j<3; j++)
+                {
+                    Console.Write($"Quais são os poderes do jogador {i+1}? ");
+                    string pwrchosen = Console.ReadLine();
+
+                    if (pwrchosen == "Fly")
+                    {
+                        pwrs[i]^=Powers.Fly;
+                    }
+                    else if (pwrchosen == "XRayVision")
+                    {
+                        pwrs[i]^=Powers.XRayVision;
+                    }
+                    else if (pwrchosen == "SuperStrength")
+                    {
+                        pwrs[i]^=Powers.SuperStrength;
+                    }
+                    else
+                        continue;
+                }
+
+                //print powers
+                Console.WriteLine($"Poderes do Jogador {i+1}: " + pwrs[i]);
+
+                //flying radiation
+                if ((pwrs[i] & Powers.Fly) == Powers.Fly && ((pwrs[i] & Powers.
+                SuperStrength) == Powers.SuperStrength))
+                {
+                    Console.WriteLine("Flying radiation!");
+                }
+            }
+
         }
     }
 }
