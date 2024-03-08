d420b97148d23641a79362693403bd49aa3298f8
João <joaombmrodrigues@gmail.com>
2024-03-08 01:17:39 +0000

    Criado projeto 'HeroPerks' com enumeration flag e perks declarados

---
+++ HeroPerks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HeroPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //starting perks
+            Perks playerPerks = 0;
+
+            //check args
+            foreach(char c in args[0])
+            {
+                switch(c)
+                {
+                    case 'w':
+                        playerPerks^= Perks.WaterBreathing;
+                        break;
+                    case 's':
+                        playerPerks^= Perks.Stealth;
+                        break;
+                    case 'a':
+                        playerPerks^= Perks.AutoHeal;
+                        break;
+                    case 'd':
+                        playerPerks^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                }
+            }
+        }
+    }
+}
